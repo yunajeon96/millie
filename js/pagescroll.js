8a0e9755f7a27865d1f14723f631e871aa0afaae
yunajeon96 <139088289+yunajeon96@users.noreply.github.com>
2023-09-21 18:11:54 +0900

    2023.09.21

---
+++ js/pagescroll.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.list p').eq(0).click(function () {
+        $('html,body').animate({ scrollTop: 1280 }, 500);
+    })
+    $('.list p').eq(1).click(function () {
+        $('html,body').animate({ scrollTop: 2500 }, 500);
+    })
+    $('.list p').eq(2).click(function () {
+        $('html,body').animate({ scrollTop: 4663 }, 500);
+    })
+    $('.list p').eq(3).click(function () {
+        $('html,body').animate({ scrollTop: 5056 }, 500);
+    })
+});
+
